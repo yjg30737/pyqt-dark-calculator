636bb5b6d37f07f9b4f36b1452d0755bcb4f52d1
Jung Gyu Yoon <yjg30737@gmail.com>
2021-11-24 15:53:42 +0900

    Fix data-files #3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
-    data_files=[('style', ['button.css', 'menu.css', 'theme.css'])],
+    data_files=[('.', ['style/button.css', 'style/menu.css', 'style/theme.css'])],
     packages=find_packages(),
     description='Dark theme calculator made with PyQt5',
     url='https://github.com/yjg30737/pyqt-dark-calculator.git',
