51c8d8892609d0e0192ae2f7f5687f1f0d63bd21
Jung Gyu Yoon <yjg30737@gmail.com>
2021-11-24 09:48:55 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyqt-dark-calculator',
+    version='0.0.1',
+    author='Jung Gyu Yoon',
+    author_email='yjg30737@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    description='Dark theme calculator made with PyQt5',
+    url='https://github.com/yjg30737/pyqt-dark-calculator.git',
+    install_requires=[
+        'PyQt5>=5.8'
+    ]
+)
