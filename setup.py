7c3a86901c3c29466222fc42470c2d7b904a1419
Jung Gyu Yoon <yjg30737@gmail.com>
2022-03-01 11:29:31 +0900

    Add calculator_pad_button.css to setup.py which was not included by mistake

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ setup(
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
-    package_data={'pyqt_dark_calculator.ico': ['calculator.svg']},
+    package_data={'pyqt_dark_calculator.ico': ['calculator.svg'],
+                  'pyqt_dark_calculator.style': ['calculator_pad_button.css']},
     description='PyQt5 dark calculator',
     url='https://github.com/yjg30737/pyqt-dark-calculator.git',
     install_requires=[
