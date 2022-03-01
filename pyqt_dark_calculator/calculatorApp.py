fe0fa06747c2209dc510541929af0d5207568fc7
Jung Gyu Yoon <yjg30737@gmail.com>
2022-03-01 11:24:28 +0900

    Set top title bar and set the calculator.svg icon

--- pyqt_dark_calculator/calculatorApp.py
+++ pyqt_dark_calculator/calculatorApp.py
@@ -15,17 +15,20 @@ class CalculatorApp(QApplication):
 
     def __setWindowStyle(self, main_window):
         main_window.setStyleSheet(getThemeStyle())  # theme
-        btns = main_window.findChildren(QPushButton)  # buttons
-        for btn in btns:
-            # check if text exists
-            if btn.text().strip() == '':
-                btn.setStyleSheet(getIconButtonStyle())  # no text - icon button style
-            else:
-                btn.setStyleSheet(getIconTextButtonStyle())  # text - icon-text button style
+
+        # btns = main_window.findChildren(QPushButton)  # buttons
+        # for btn in btns:
+        #     # check if text exists
+        #     if btn.text().strip() == '':
+        #         btn.setStyleSheet(getIconButtonStyle())  # no text - icon button style
+        #     else:
+        #         btn.setStyleSheet(getIconTextButtonStyle())  # text - icon-text button style
+
         menu_bar = main_window.menuBar()  # menu bar
         menu_bar_style = getMenuBarStyle(menu_bar)
         menu_bar.setStyleSheet(menu_bar_style)
 
     def __setCustomTitleBar(self, main_window):
         self.__titleBarWindow = CustomTitlebarWindow(main_window)
-        self.__titleBarWindow.setButtons(icon_filename='ico/calculator.svg')
+        self.__titleBarWindow.setTopTitleBar(icon_filename='ico/calculator.svg')
+        self.__titleBarWindow.setButtons()
