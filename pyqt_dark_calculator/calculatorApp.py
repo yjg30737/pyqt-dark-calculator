60b1ed201409623e7aec2bf3ef5ba5afae1bf6a4
Jung Gyu Yoon <yjg30737@gmail.com>
2022-03-01 11:08:31 +0900

    Add calculatorApp.py, add packages(pyqt-dark-gray-theme, pyqt-custom-titlebar-window)

---
+++ pyqt_dark_calculator/calculatorApp.py
@@ -0,0 +1,31 @@
+from PyQt5.QtWidgets import QApplication
+from pyqt_custom_titlebar_window import CustomTitlebarWindow
+from pyqt_dark_gray_theme import *
+from pyqt_dark_calculator import Calculator
+
+
+class CalculatorApp(QApplication):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        mainWindow = Calculator()
+        self.__titleBarWindow = ''
+        self.__setWindowStyle(mainWindow)
+        self.__setCustomTitleBar(mainWindow)
+        self.__titleBarWindow.show()
+
+    def __setWindowStyle(self, main_window):
+        main_window.setStyleSheet(getThemeStyle())  # theme
+        btns = main_window.findChildren(QPushButton)  # buttons
+        for btn in btns:
+            # check if text exists
+            if btn.text().strip() == '':
+                btn.setStyleSheet(getIconButtonStyle())  # no text - icon button style
+            else:
+                btn.setStyleSheet(getIconTextButtonStyle())  # text - icon-text button style
+        menu_bar = main_window.menuBar()  # menu bar
+        menu_bar_style = getMenuBarStyle(menu_bar)
+        menu_bar.setStyleSheet(menu_bar_style)
+
+    def __setCustomTitleBar(self, main_window):
+        self.__titleBarWindow = CustomTitlebarWindow(main_window)
+        self.__titleBarWindow.setButtons()
