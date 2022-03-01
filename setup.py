dbc222a0a58839abeca0cf8161879a15eddd2724
Jung Gyu Yoon <yjg30737@gmail.com>
2022-03-01 11:11:51 +0900

    Fix typo

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ setup(
     url='https://github.com/yjg30737/pyqt-dark-calculator.git',
     install_requires=[
         'PyQt5>=5.15',
-        'pyqt-dark-gray-theme @ git+https://git@github.com/yjg30737/pyqt-dark-gray-theme.git@main'
-        'pyqt-custom-titlebar-window @ git+https://git@github.com/yjg30737/pyqt-custom-titlebar-window.git@main'
+        'pyqt-dark-gray-theme @ git+https://git@github.com/yjg30737/pyqt-dark-gray-theme.git@main',
+        'pyqt-custom-titlebar-window @ git+https://git@github.com/yjg30737/pyqt-custom-titlebar-window.git@main',
         'pyqt-resource-helper @ git+https://git@github.com/yjg30737/pyqt-resource-helper.git@main'
     ]
 )
