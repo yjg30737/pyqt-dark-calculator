5ddfcce047d68c7d5d28d31ea4ee60439f5fe572
Jung Gyu Yoon <yjg30737@gmail.com>
2022-03-02 12:47:48 +0900

    Use new packages

--- pyqt_dark_calculator/calculatorApp.py
+++ pyqt_dark_calculator/calculatorApp.py
@@ -1,36 +1,15 @@
-import os, sys, inspect
-
-from PyQt5.QtWidgets import QApplication, QPushButton
-from pyqt_custom_titlebar_window import CustomTitlebarWindow
+from PyQt5.QtWidgets import QApplication, QAbstractButton
 from pyqt_dark_gray_theme.darkGrayTheme import *
 from pyqt_dark_calculator.calculator import Calculator
+
+from pyqt_style_setter import StyleSetter
+from pyqt_custom_titlebar_setter import CustomTitlebarSetter
 
 
 class CalculatorApp(QApplication):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         mainWindow = Calculator()
-        self.__setWindowStyle(mainWindow)
-        self.__setCustomTitleBar(mainWindow)
-
-    def __setWindowStyle(self, main_window):
-        main_window.setStyleSheet(getThemeStyle())  # theme
-
-        # btns = main_window.findChildren(QPushButton)  # buttons
-        # for btn in btns:
-        #     # check if text exists
-        #     if btn.text().strip() == '':
-        #         btn.setStyleSheet(getIconButtonStyle())  # no text - icon button style
-        #     else:
-        #         btn.setStyleSheet(getIconTextButtonStyle())  # text - icon-text button style
-
-        menu_bar = main_window.menuBar()  # menu bar
-        menu_bar_style = getMenuBarStyle(menu_bar)
-        menu_bar.setStyleSheet(menu_bar_style)
-
-    def __setCustomTitleBar(self, main_window):
-        self.__titleBarWindow = CustomTitlebarWindow(main_window)
-        caller_path = os.path.dirname(inspect.getframeinfo(sys._getframe(1)).filename)
-        self.__titleBarWindow.setTopTitleBar(icon_filename=os.path.join(caller_path, 'ico\\calculator.svg'))
-        self.__titleBarWindow.setButtons()
+        StyleSetter.setWindowStyle(mainWindow, exclude_type_lst=[QAbstractButton])
+        self.__titleBarWindow = CustomTitlebarSetter.getCustomTitleBar(mainWindow, icon_filename='ico/calculator.svg')
         self.__titleBarWindow.show()
