f03ffdfbd45c9a430f2bbef32ca64ced33a38976
Jung Gyu Yoon <yjg30737@gmail.com>
2022-03-08 10:04:42 +0900

    Update version (0.3.2 -> 0.3.3)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyqt-dark-calculator',
-    version='0.3.2',
+    version='0.3.3',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
