75e43cc22ded3b10cf3bb69b90e35adc1cfe492a
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-12 14:31:20 +0900

    Update packages' url

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ setup(
     packages=find_packages(),
     package_data={'pyqt_dark_calculator.ico': ['calculator.svg'],
                   'pyqt_dark_calculator.style': ['calculator_pad_button.css']},
-    description='PyQt Dark Calculator',
+    description='PyQt dark calculator',
     url='https://github.com/yjg30737/pyqt-dark-calculator.git',
     install_requires=[
         'PyQt5>=5.15',
-        'pyqt-style-setter @ git+https://git@github.com/yjg30737/pyqt-style-setter.git@main',
-        'pyqt-resource-helper @ git+https://git@github.com/yjg30737/pyqt-resource-helper.git@main',
-        'python-get-absolute-resource-path @ git+https://git@github.com/yjg30737/python-get-absolute-resource-path.git@main',
+        'pyqt-style-setter>=0.0.1',
+        'pyqt-resource-helper>=0.0.1',
+        'python-get-absolute-resource-path>=0.0.1',
         'pyqt-new-window-handler @ git+https://git@github.com/yjg30737/pyqt-new-window-handler.git@main'
     ]
 )
