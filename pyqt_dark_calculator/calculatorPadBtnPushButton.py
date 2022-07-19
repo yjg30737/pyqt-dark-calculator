d51543efce2d8c8584bd28f7e8d323a948af8bf6
Jung Gyu Yoon <yjg30737@gmail.com>
2022-07-19 09:15:24 +0900

    Increase font size of pad button

--- pyqt_dark_calculator/calculatorPadBtnPushButton.py
+++ pyqt_dark_calculator/calculatorPadBtnPushButton.py
@@ -7,6 +7,12 @@ class CalculatorPadBtnPushButton(QPushButton):
 
     def __init__(self, text):
         super().__init__(text)
+        self.__initUi()
+
+    def __initUi(self):
+        font = self.font()
+        font.setPixelSize(20)
+        self.setFont(font)
 
     @pyqtSlot()
     def mousePressEvent(self, e):
